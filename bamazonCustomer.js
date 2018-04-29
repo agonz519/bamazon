af3c7aa7d940cf8128ed75fc044ac4b1654df3d4
Alexander <agonz519@gmail.com>
2018-04-29 17:54:36 -0400

    Built functionality for creating an array of items for sale, then using the array to populate choices for an inquirer list. Had to figure out how to use Promises. Almost rage quit. Almost.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-const mysql = require("mysql");
-const cTable = require("console.table");
+const mysql = require('mysql');
+const cTable = require('console.table');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
@@ -12,14 +13,77 @@ const connection = mysql.createConnection({
 
 connection.connect(function(error) {
   if (error) throw 'An error has occurred while connecting - ' + error;
-  console.log("connected as id " + connection.threadId);
-  selectAll();
+  console.log('connected as id ' + connection.threadId);
+  greetCustomer();
   connection.end();
 });
 
-function selectAll() {
-  connection.query('SELECT * FROM bamazon.products', function(error, response) {
+let queryItemsForSale = new Promise((resolve, reject) => {
+  connection.query('SELECT * FROM bamazon.products', function (error, response) {
     if (error) throw 'An error has occurred with selecting data - ' + error;
-    console.table(response);
+    let productArray = [],
+      arrayItem = '';
+    response.forEach((item) => {
+      arrayItem = item.item_id + ': ' + item.product_name + ' ----- $' + item.price;
+      productArray.push(arrayItem);
+    });
+    resolve(productArray);
   });
-}
+});
+
+let greetCustomer = () => {
+  queryItemsForSale.then((productArray) => {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'product',
+        message: 'Shopkeeper: Hello adventurer! What item would you like to purchase to help you on your journey?',
+        choices: productArray
+      }
+    ]).then((answer) => {
+      console.log('You chose ' + answer.product);
+      let tempArray = answer.product.split(':');
+      let id = tempArray[0];
+      console.log('The ID for this item is ' + id);
+    })
+  });
+};
+
+
+//
+// function queryItemsForSale(callback) {
+//   connection.query('SELECT * FROM bamazon.products', function(error, response) {
+//     if (error) throw 'An error has occurred with selecting data - ' + error;
+//     // console.log(response);
+//     let productArray = [];
+//     let arrayItem = '';
+//     response.forEach((item) => {
+//       arrayItem = item.item_id + ': ' + item.product_name + ' ----- $' + item.price;
+//       productArray.push(arrayItem);
+//     });
+//     return productArray;
+//   });
+//   callback();
+// }
+//
+// let greetCustomer = () => {
+//   queryItemsForSale(
+//     inquirer.prompt([
+//       {
+//         type: 'list',
+//         name: 'product',
+//         message: 'Shopkeeper: Hello adventurer! What item would you like to purchase to help you on your journey?',
+//         choices: productArray
+//       }
+//     ]).then((answer) => {
+//       console.log('please work' + answer.product);
+//     })
+//   );
+//
+// };
+//
+//
+// //
+// // let greetCustomer () => {
+//
+// // }
