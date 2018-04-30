5b74fb5e7eee944c62e05920f9d07ff4496c8328
Alexander <agonz519@gmail.com>
2018-04-30 00:22:57 -0400

    Completed minimum requirement

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,7 +15,7 @@ connection.connect(function(error) {
   if (error) throw 'An error has occurred while connecting - ' + error;
   console.log('connected as id ' + connection.threadId);
   greetCustomer();
-  connection.end();
+
 });
 
 let queryItemsForSale = new Promise((resolve, reject) => {
@@ -39,51 +39,47 @@ let greetCustomer = () => {
         name: 'product',
         message: 'Shopkeeper: Hello adventurer! What item would you like to purchase to help you on your journey?',
         choices: productArray
+      },
+      {
+        type: 'input',
+        name: 'units',
+        message: 'How many units would you like to purchase?'
       }
     ]).then((answer) => {
-      console.log('You chose ' + answer.product);
       let tempArray = answer.product.split(':');
       let id = tempArray[0];
-      console.log('The ID for this item is ' + id);
+      connection.query('SELECT stock_quantity, price FROM bamazon.products WHERE item_id = ?', [id], function (error, response, fields) {
+        if (error) throw 'An error has occurred with selecting quantity where ID equals ' + id + ' - ' + error;
+        let newQuantity = response[0].stock_quantity - parseInt(answer.units);
+        let total = response[0].price * answer.units;
+        if (newQuantity > -1) {
+          connection.query('UPDATE bamazon.products SET stock_quantity = ? WHERE item_id = ?', [newQuantity, id], function (error, response, fields) {
+            if (error) throw 'An error has occurred with updating the quantity - ' + error;
+            console.log('Shopkeeper: Your total came out to $' + total + '.');
+            continueShopping();
+          });
+        } else {
+          console.log('\nShopkeeper: Sorry, we are out of stock of that item!\n');
+          continueShopping();
+        }
+      });
     })
   });
 };
 
-
-//
-// function queryItemsForSale(callback) {
-//   connection.query('SELECT * FROM bamazon.products', function(error, response) {
-//     if (error) throw 'An error has occurred with selecting data - ' + error;
-//     // console.log(response);
-//     let productArray = [];
-//     let arrayItem = '';
-//     response.forEach((item) => {
-//       arrayItem = item.item_id + ': ' + item.product_name + ' ----- $' + item.price;
-//       productArray.push(arrayItem);
-//     });
-//     return productArray;
-//   });
-//   callback();
-// }
-//
-// let greetCustomer = () => {
-//   queryItemsForSale(
-//     inquirer.prompt([
-//       {
-//         type: 'list',
-//         name: 'product',
-//         message: 'Shopkeeper: Hello adventurer! What item would you like to purchase to help you on your journey?',
-//         choices: productArray
-//       }
-//     ]).then((answer) => {
-//       console.log('please work' + answer.product);
-//     })
-//   );
-//
-// };
-//
-//
-// //
-// // let greetCustomer () => {
-//
-// // }
+function continueShopping() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'continue',
+      message: 'Shopkeeper: Would you like to continue shopping? (y/n)'
+    }
+  ]).then((answer) => {
+    if (answer.continue.toLowerCase() === 'y') {
+      greetCustomer();
+    } else {
+      console.log('\nThanks for shopping at BAMAZON, the adventurer\'s one stop shop!\n');
+      connection.end();
+    }
+  });
+}
